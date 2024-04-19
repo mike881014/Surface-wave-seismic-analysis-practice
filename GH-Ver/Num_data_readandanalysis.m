e2dd7ba434fac5c755eee201f79f2d2875ee6970
mike881014 <bang8875e@gmail.com>
2024-04-19 21:02:55 +0800

    Only Vertical

--- GH-Ver/Num_data_readandanalysis.m
+++ GH-Ver/Num_data_readandanalysis.m
@@ -1,5 +1,5 @@
 clear all,close all,clc
-cd('D:\模擬\學姊\');
+cd('D:\模擬\Surface-wave-seismic-analysis-practice\GH-Ver');
 % In the practicing now:
 % Z: vertical direction, 
 % X: radial direction, 
@@ -17,19 +17,11 @@ Direction_Z='Z';fn3_Z=['.FX',Direction_Z,'.semd'];
 
 for i=1:1:NF % 
     fn2=num2str(first_fnu+i-1); % The following lines putting the pieces of file names together
-    FileName_X=[fn1 fn2 fn3_X]; % 彙整檔案名 (X)
-    FileName_Y=[fn1 fn2 fn3_Y]; % (Y)
     FileName_Z=[fn1 fn2 fn3_Z]; % (Z)
 
-    RawData_X=load(FileName_X); % 讀入X_Y_Z檔案
-    RawData_Y=load(FileName_Y);
     RawData_Z=load(FileName_Z);
     
     % 不太懂 是為了讓資料不要是負的嗎? 為何需要這步
-    y_X(:,i)=-RawData_X(:,2); % Add negitive sign for direction correction
-    y_X_norm(:,i) = y_X(:,i)/max(abs(y_X(:,i))); % 正規化
-    y_Y(:,i)=-RawData_Y(:,2); % Add negitive sign for direction correction
-    y_Y_norm(:,i) = y_Y(:,i)/max(abs(y_Y(:,i))); % 正規化
     y_Z(:,i)=RawData_Z(:,2);
     y_Z_norm(:,i) = y_Z(:,i)/max(abs(y_Z(:,i)));% 正規化
 
@@ -41,7 +33,7 @@ dx=0.1; % Geophone spacing
 x0=0.5; % distance between source & 1st receiver
 x=[x0:dx:x0+(Ch-1)*dx]'; % 標出每個測站位置
 
-t = RawData_X(:,1); dt=t(2)-t(1); % time step t:每筆資料的時間 dt:時間間隔 
+t = RawData_Z(:,1); dt=t(2)-t(1); % time step t:每筆資料的時間 dt:時間間隔 
 % [m,t0_ind] = min(abs(t)); % Time correction, the simulation time start from negitive time 
 % t = t(t0_ind:end); N = length(t);
 % y_X = y_X(t0_ind:end,:); y_Y = y_Y(t0_ind:end,:); y_Z = y_Z(t0_ind:end,:);
@@ -49,19 +41,13 @@ N = length(t); t=[0:dt:(N-1)*dt]; % [起始:跌代值:終值]
 
 %% Figure plot in time domain
 figure,
-subplot(131),wigb(y_Z_norm,1,x,t); % plot seismic data in wiggle format
+wigb(y_Z_norm,1,x,t); % plot seismic data in wiggle format
 set(gca,'FontSize',10,'Ydir','reverse');ylabel('Time (s)'); xlabel('Offset (m)'); title('Vertical component')
-subplot(132),wigb(y_X_norm,1,x,t);
-set(gca,'FontSize',10,'Ydir','reverse');ylabel('Time (s)'); xlabel('Offset (m)'); title('Radial component')
-subplot(133),wigb(y_Y_norm,1,x,t);
-set(gca,'FontSize',10,'Ydir','reverse');ylabel('Time (s)'); xlabel('Offset (m)'); title('Transversal component')
 
 %% Zero padding to 1 sec
 % 目前這邊的理解是 將資料拓展成以頻率為基準的資料集
 N = round(1/dt);t = [0:dt:(N-1)*dt]; % 將頻率算出 N:f
-data_length = length(y_X);
-y_X = [y_X;zeros(N-data_length,length(x))];
-y_Y = [y_Y;zeros(N-data_length,length(x))];
+data_length = length(y_Z);
 y_Z = [y_Z;zeros(N-data_length,length(x))];
 
 
@@ -78,88 +64,69 @@ y_Z = [y_Z;zeros(N-data_length,length(x))];
 
 %% Dispersion analysis
 % Analysis frequency range
-fmin=0; fmax=14000; df=10;
+fmin=0; fmax=14000; df=1;
 % Analysis velocity range
-vmin = 0; vmax=2000; dv=10;
+vmin = 0; vmax=2000; dv=1;
 % Analysis wave length range
-lmin=0; lmax=100; dl=0.5;
+lmin=0; lmax=100; dl=1;
 % Analysis wave number range
-kmin=-80; kmax=80; dk=0.05;
+kmin=-80; kmax=80; dk=1;
 
 % Peform f-v transform, f-v
-
-[f,v,A_Z,Z_fx]=fv(y_Z,dt,x,fmin,fmax,df,vmin,vmax,dv);
-[f,v,A_X,X_fx]=fv(y_X,dt,x,fmin,fmax,df,vmin,vmax,dv);
-[f,v,A_Y,Y_fx]=fv(y_Y,dt,x,fmin,fmax,df,vmin,vmax,dv);
+[f,v,A_Z_v,Z_fx]=fv(y_Z,dt,x,fmin,fmax,df,vmin,vmax,dv);
 
 % Peform f-k transform, f-k
-% [f,v,A_Z,Z_fx]=fk(y_Z,dt,x,fmin,fmax,df,kmin,kmax,dk);
-% [f,v,A_X,X_fx]=fk(y_X,dt,x,fmin,fmax,df,kmin,kmax,dk);
-% [f,v,A_Y,Y_fx]=fk(y_Y,dt,x,fmin,fmax,df,kmin,kmax,dk);
+[f,k,A_Z_k,Z_fk]=fk(y_Z,dt,x,fmin,fmax,df,kmin,kmax,dk);
 
 % Perform f-l transform and then transform to l-v
 [f,l,Al_Z]=fl(y_Z,dt,x,fmin,fmax,df,lmin,lmax,dl);[l,v, A_lv_Z]=fl_to_lv(Al_Z, f, l, v);
-[f,l,Al_X]=fl(y_X,dt,x,fmin,fmax,df,lmin,lmax,dl);[l,v, A_lv_X]=fl_to_lv(Al_X, f, l, v);
-[f,l,Al_Y]=fl(y_Y,dt,x,fmin,fmax,df,lmin,lmax,dl);[l,v, A_lv_Y]=fl_to_lv(Al_Y, f, l, v);
 
 
 fe=f;
-Vphe_Z=zeros(length(fe),1);Vphe_X=zeros(length(fe),1);Vphe_Y=zeros(length(fe),1);
+Vphe_Z_v=zeros(length(fe),1);
 for i=1:length(fe);
-   [Amax Ai]=max(A_Z(i,:));
-   Vphe_Z(i)=v(Ai);
-   An_Z(i,:) = A_Z(i,:)/Amax;
-
-   [Amax Ai]=max(A_X(i,:));
-   Vphe_X(i)=v(Ai);
-   An_X(i,:) = A_X(i,:)/Amax;
-
-   [Amax Ai]=max(A_Y(i,:));
-   Vphe_Y(i)=v(Ai);
-   An_Y(i,:) = A_Y(i,:)/Amax;
+   [Amax_v Ai_v]=max(A_Z_v(i,:));
+   Vphe_Z_v(i)=v(Ai_v);
+   An_Z_v(i,:) = A_Z_v(i,:)/Amax_v;
 end
 
-% for i=1:length(fe);
-%    [Amax Ai]=max(Ak_Z(i,:));
-%    Vphe_Z(i)=v(Ai);
-%    An_Z_k(i,:) = Ak_Z(i,:)/Amax;
+Vphe_Z_k=zeros(length(fe),1);
+for i=1:length(fe);
+   [Amax_k Ai_k]=max(A_Z_k(i,:));
+   Vphe_Z_k(i)=k(Ai_k);
+   An_Z_k(i,:) = A_Z_k(i,:)/Amax_k;
+end
+
+
+%% Radial to vertical relation (sRVSR, RVPD and RVSR) f-v version.
+% CWU=conj(Z_fx).*X_fx;
+% RVSR = abs(X_fx)./abs(Z_fx); 
+% RVPD=angle(CWU); 
+% sRVSR=sign(imag(CWU)).*RVSR;  
 % 
-%    [Amax Ai]=max(Ak_X(i,:));
-%    Vphe_X(i)=v(Ai);
-%    An_X_k(i,:) = Ak_X(i,:)/Amax;
-% 
-%    [Amax Ai]=max(Ak_Y(i,:));
-%    Vphe_Y(i)=v(Ai);
-%    An_Y_k(i,:) = Ak_Y(i,:)/Amax;
-% end
-
-
-%% Radial to vertical relation (sRVSR, RVPD and RVSR)
-CWU=conj(Z_fx).*X_fx;
-RVSR = abs(X_fx)./abs(Z_fx); 
-RVPD=angle(CWU); 
-sRVSR=sign(imag(CWU)).*RVSR;  
-
-% Subplot: sRVSR, RVPD, RVSR
-figure;
-% subplot(131),
-pcolor(x,f,sRVSR); 
-shading interp;
-set(gca,'layer','top','Box','on', 'FontSize',12,'Ydir','reverse');
-colormap(gca,jet);colorbar;caxis([-2 2]);
-title('sRVSR'); xlabel('x (m)'); ylabel('f (Hz)');
-
-% subplot(132),pcolor(x,f,RVPD);
+% % Subplot: sRVSR, RVPD, RVSR
+% figure;
+% % subplot(131),
+% pcolor(x,f,sRVSR); 
 % shading interp;
 % set(gca,'layer','top','Box','on', 'FontSize',12,'Ydir','reverse');
-% colormap(gca,jet);colorbar;caxis([-pi pi]);
-% title('RVPD'); xlabel('x (m)'); ylabel('f (Hz)');
+% colormap(gca,jet);colorbar;caxis([-2 2]);
+% title('sRVSR'); xlabel('x (m)'); ylabel('f (Hz)');
+
+%% Radial to vertical relation (sRVSR, RVPD and RVSR) f-k version.
+% CWU=conj(Z_fk).*X_fx;
+% RVSR = abs(X_fk)./abs(Z_fk); 
+% RVPD=angle(CWU); 
+% sRVSR=sign(imag(CWU)).*RVSR;  
 % 
-% subplot(133),pcolor(x,f,RVSR);
+% % Subplot: sRVSR, RVPD, RVSR
+% figure;
+% % subplot(131),
+% pcolor(x,f,sRVSR); 
 % shading interp;
-% set(gca,'layer','top','Box','on', 'FontSize',12,'Ydir','reverse','ColorScale','log');
-% colormap(gca,gray);colorbar;caxis([-2 2]);
-% title('RVSR'); xlabel('x (m)'); ylabel('f (Hz)');
+% set(gca,'layer','top','Box','on', 'FontSize',12,'Ydir','reverse');
+% colormap(gca,jet);colorbar;caxis([-2 2]);
+% title('sRVSR'); xlabel('x (m)'); ylabel('f (Hz)');
 
 %% Plot in f-x domain
 % figure,
@@ -185,26 +152,23 @@ title('sRVSR'); xlabel('x (m)'); ylabel('f (Hz)');
 % title('Transversal'); xlabel('x (m)'); ylabel('f (Hz)');
 
 
-%% Plot in fv domain
+%% Plot in f-v domain
 figure;
 % subplot(131),imagesc(f,v,An_Z);colormap(jet);
-imagesc(f, v, An_Z);colormap(jet); % 先別加 subplot，圖會變小張 4/13
+imagesc(f, v, An_Z_v);colormap(jet); % 先別加 subplot，圖會變小張 4/13
 % cs=0.5; cmap=(meshgrid(1:-1/63:0,1:3)').^cs;colormap(cmap);
-hold on;plot(Vphe_Z,fe,'w.','Markersize',10);
-ylabel('Phase Velocity, V_{ph} (m/s)');xlabel('Frequency, f (Hz)'); title('Vertical')
+hold on;plot(Vphe_Z_v,fe,'w.','Markersize',10);
+ylabel('Phase Velocity, V_{ph} (m/s)');xlabel('Frequency, f (Hz)'); title('F-V Vertical')
 set(gca,'fontsize',14,'YDir', 'normal');
 
-% subplot(132),imagesc(v,f,An_X);colormap(jet);
-% % cs=0.5; cmap=(meshgrid(1:-1/63:0,1:3)').^cs;colormap(cmap);
-% hold on;plot(Vphe_X,fe,'w.','Markersize',10);
-% xlabel('Phase Velocity, V_{ph} (m/s)');ylabel('Frequency, f (Hz)'); title('Radial')
-% set(gca,'fontsize',14);
-% 
-% subplot(133),imagesc(v,f,An_Y);colormap(jet);
-% % cs=0.5; cmap=(meshgrid(1:-1/63:0,1:3)').^cs;colormap(cmap);
-% hold on;plot(Vphe_Y,fe,'w.','Markersize',10);
-% xlabel('Phase Velocity, V_{ph} (m/s)');ylabel('Frequency, f (Hz)'); title('Transversal')
-% set(gca,'fontsize',14);
+%% Plot in f-k domain
+figure;
+% subplot(131),imagesc(f,v,An_Z);colormap(jet);
+imagesc(f, k, An_Z_k);colormap(jet); % 先別加 subplot，圖會變小張 4/13
+% cs=0.5; cmap=(meshgrid(1:-1/63:0,1:3)').^cs;colormap(cmap);
+hold on;plot(Vphe_Z_k,fe,'w.','Markersize',10);
+ylabel('Phase Velocity, V_{ph} (m/s)');xlabel('Frequency, f (Hz)'); title('F-K Vertical')
+set(gca,'fontsize',14,'YDir', 'normal');
 
 %% Plot in l-v domain
 % figure;
